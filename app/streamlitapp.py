05eae6427827f620a480fcac0fe2d92f66a7f2eb
Amith A G <118121039+Amith-AG@users.noreply.github.com>
2023-06-15 14:23:08 +0530

    Update streamlitapp.py

--- app/streamlitapp.py
+++ app/streamlitapp.py
@@ -1,7 +1,8 @@
 # Import all of the dependencies
 import streamlit as st
 import os 
-from moviepy.editor import VideoFileClip
+import imageio 
+
 import tensorflow as tf 
 from utils import load_data, num_to_char
 from modelutil import load_model
@@ -17,7 +18,7 @@ with st.sidebar:
 
 st.markdown("<h1 style='text-align: center; color: white;'>LipNet</h1>", unsafe_allow_html=True) 
 # Generating a list of options or videos 
-options = os.listdir('data/s1')
+options = os.listdir(os.path.join('..', 'data', 's1'))
 selected_video = st.selectbox('Choose video', options)
 
 # Generate two columns 
@@ -28,22 +29,18 @@ if options:
     # Rendering the video 
     with col1: 
         st.info('The video below displays the converted video in mp4 format')
-        file_path = os.path.join('data', 's1', selected_video.replace('\\', '/'))
-        output_file_path = 'test_video.mp4'
-    
-    # Convert video format using moviepy
-        video = VideoFileClip(file_path)
-        video.write_videofile(output_file_path, codec='libx264', audio_codec='aac')
+        file_path = os.path.join('..','data','s1', selected_video)
+        os.system(f'ffmpeg -i {file_path} -vcodec libx264 test_video.mp4 -y')
 
-    # Rendering inside the app
-        video = open(output_file_path, 'rb')
-        video_bytes = video.read()
+        # Rendering inside of the app
+        video = open('test_video.mp4', 'rb') 
+        video_bytes = video.read() 
         st.video(video_bytes)
 
 
     with col2: 
         st.info('This is all the machine learning model sees when making a prediction')
-        video, annotations = load_data(file_path)
+        video, annotations = load_data(tf.convert_to_tensor(file_path))
         imageio.mimsave('animation.gif', video, fps=10)
         st.image('animation.gif', width=400) 
 
