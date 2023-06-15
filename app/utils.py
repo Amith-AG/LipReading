5649498a704fbf71a510de36bc609c2e91b689c7
Amith A G <118121039+Amith-AG@users.noreply.github.com>
2023-06-15 14:21:18 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -34,14 +34,12 @@ def load_alignments(path:str) -> List[str]:
         if line[2] != 'sil': 
             tokens = [*tokens,' ',line[2]]
     return char_to_num(tf.reshape(tf.strings.unicode_split(tokens, input_encoding='UTF-8'), (-1)))[1:]
-
 def load_data(path: str): 
-    path = bytes.decode(path.numpy())
     file_name = path.split('/')[-1].split('.')[0]
-    # File name splitting for windows
+    # File name splitting for Windows
     file_name = path.split('\\')[-1].split('.')[0]
-    video_path = os.path.join('..','data','s1',f'{file_name}.mpg')
-    alignment_path = os.path.join('..','data','alignments','s1',f'{file_name}.align')
+    video_path = os.path.join('..', 'data', 's1', f'{file_name}.mpg')
+    alignment_path = os.path.join('..', 'data', 'alignments', 's1', f'{file_name}.align')
     frames = load_video(video_path) 
     alignments = load_alignments(alignment_path)
     return frames, alignments
