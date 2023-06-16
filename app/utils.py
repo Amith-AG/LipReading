3d5b9625834d30e5d935b6f535b1514cfe7bc27d
Amith A G <118121039+Amith-AG@users.noreply.github.com>
2023-06-16 10:40:19 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -37,9 +37,11 @@ def load_alignments(path:str) -> List[str]:
 
 def load_data(path: str): 
     path = bytes.decode(path.numpy())
-    file_name = os.path.basename(path).split('.')[0]
-    video_path = os.path.join('/data/s1', f'{file_name}.mpg')
-    alignment_path = os.path.join('/data/alignments/s1', f'{file_name}.align')
+    file_name = path.split('/')[-1].split('.')[0]
+    # File name splitting for windows
+    file_name = path.split('\\')[-1].split('.')[0]
+    video_path = os.path.join('..','data','s1',f'{file_name}.mpg')
+    alignment_path = os.path.join('..','data','alignments','s1',f'{file_name}.align')
     frames = load_video(video_path) 
     alignments = load_alignments(alignment_path)
     return frames, alignments
