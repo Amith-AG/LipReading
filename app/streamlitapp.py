5f47cd532a39452404b9e74e97ed26a5c48c2012
Amith A G <118121039+Amith-AG@users.noreply.github.com>
2023-06-16 17:18:31 +0530

    Update streamlitapp.py

--- app/streamlitapp.py
+++ app/streamlitapp.py
@@ -52,11 +52,8 @@ if options:
     with col2: 
         st.info('This is all the machine learning model sees when making a prediction')
         video, annotations = load_data(tf.convert_to_tensor(file_path))
-        st.info(video)
-        st.info(annotations)
-#         st.info(path)
-#         imageio.mimsave('animation.gif', video, fps=10)
-#         st.image('animation.gif', width=400) 
+        imageio.mimsave('animation.gif', video, fps=10)
+        st.image('animation.gif', width=400) 
 
         st.info('This is the output of the machine learning model as tokens')
         model = load_model()
