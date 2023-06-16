756141f54303cfe76151f91abe3d8506f083cbba
Amith A G <118121039+Amith-AG@users.noreply.github.com>
2023-06-16 15:56:15 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -37,7 +37,6 @@ def load_alignments(path:str) -> List[str]:
 
 def load_data(path: str): 
     path = bytes.decode(path.numpy())
-    file_name = os.path.splitext(os.path.basename(path))[0]
     # File name splitting for windows
     file_name = path.split('/')[-1].split('.')[0]
     video_path = '/app/lipreading/data/s1' + f'{file_name}.mpg'
