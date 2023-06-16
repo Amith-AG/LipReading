b400202f6e3bb1c8a7c3edd2f168ce0534efdf5b
Amith A G <118121039+Amith-AG@users.noreply.github.com>
2023-06-16 15:28:37 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -37,15 +37,12 @@ def load_alignments(path:str) -> List[str]:
 
 def load_data(path: str): 
     path = bytes.decode(path.numpy())
-    # print("path=",path)
     file_name = path.split('/')[-1].split('.')[0]
     # File name splitting for windows
     file_name = path.split('\\')[-1].split('.')[0]
-    # print("filename=",file_name)
-    video_path = r'/app/lipreading/data/s1' + f'{file_name}.mpg'
-    # print("vid_path",video_path)
-    alignment_path =r'/app/lipreading/data/alignments/s1/'+f'{file_name}.align'
-    # print("align_path",alignment_path)
-    frames = load_video(video_path) 
-    alignments = load_alignments(alignment_path)
-    return frames, alignments
+    video_path = '/app/lipreading/data/s1' + f'{file_name}.mpg'
+    alignment_path ='/app/lipreading/data/alignments/s1/'+f'{file_name}.align'
+
+#     frames = load_video(video_path) 
+#     alignments = load_alignments(alignment_path)
+    return "path=",path," file_name=",file_name," video_path=",video_path," algin_path=",alignment_path
