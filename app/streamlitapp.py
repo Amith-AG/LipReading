e73adeb561fbb1b0212c9e868ed9d8f7da9fb51b
Amith A G <118121039+Amith-AG@users.noreply.github.com>
2023-06-17 10:03:32 +0530

    Update streamlitapp.py strat from the beginning

--- app/streamlitapp.py
+++ app/streamlitapp.py
@@ -1,13 +1,13 @@
+# Import all of the dependencies
 import streamlit as st
 import os 
-import pathlib
-from moviepy.editor import VideoFileClip
-import imageio
+import imageio 
+
 import tensorflow as tf 
 from utils import load_data, num_to_char
 from modelutil import load_model
 
-# Set the layout of the Streamlit app as wide 
+# Set the layout to the streamlit app as wide 
 st.set_page_config(layout='wide')
 
 # Setup the sidebar
@@ -17,15 +17,8 @@ with st.sidebar:
     st.info('This project, developed by Amith A G as his MCA final project at KVVS Institute Of Technology, focuses on implementing the LipNet deep learning model for lip-reading and speech recognition. The project aims to demonstrate the capabilities of the LipNet model through a Streamlit application.')
 
 st.markdown("<h1 style='text-align: center; color: white;'>LipNet</h1>", unsafe_allow_html=True) 
-
 # Generating a list of options or videos 
-code_dir = pathlib.Path(__file__).parent.resolve()
-files_location = code_dir / ".." / "data" / "s1"  
-files_location = files_location.resolve()  
-
-# Convert the files_location to a list of files
-options = os.listdir(files_location)
-
+options = os.listdir(os.path.join('..', 'data', 's1'))
 selected_video = st.selectbox('Choose video', options)
 
 # Generate two columns 
@@ -36,23 +29,18 @@ if options:
     # Rendering the video 
     with col1: 
         st.info('The video below displays the converted video in mp4 format')
-        file_path = str(files_location / selected_video)
-        output_path = str(code_dir / 'test_video.mp4')
-    
-        # Convert the video using moviepy
-        video_clip = VideoFileClip(file_path)
-        video_clip.write_videofile(output_path, codec='libx264')
-    
-        # Display the video in the app
-        video = open(output_path, 'rb')
-        video_bytes = video.read()
+        file_path = os.path.join('..','data','s1', selected_video)
+        os.system(f'ffmpeg -i {file_path} -vcodec libx264 test_video.mp4 -y')
+
+        # Rendering inside of the app
+        video = open('test_video.mp4', 'rb') 
+        video_bytes = video.read() 
         st.video(video_bytes)
 
 
     with col2: 
         st.info('This is all the machine learning model sees when making a prediction')
         video, annotations = load_data(tf.convert_to_tensor(file_path))
-        st.info(video)
         imageio.mimsave('animation.gif', video, fps=10)
         st.image('animation.gif', width=400) 
 
@@ -66,3 +54,4 @@ if options:
         st.info('Decode the raw tokens into words')
         converted_prediction = tf.strings.reduce_join(num_to_char(decoder)).numpy().decode('utf-8')
         st.text(converted_prediction)
+        
